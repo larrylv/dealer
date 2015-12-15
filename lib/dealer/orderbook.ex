7bb95aa6454e100af64ef5e4de236d9406527eba
Larry Lv <larrylv1990@gmail.com>
2015-12-15 19:13:26 +0800

    Add Orderbook struct

---
+++ lib/dealer/orderbook.ex
@@ -0,0 +1,25 @@
+defmodule Dealer.Stock.Orderbook do
+  @moduledoc """
+  Struct of an orderbook for a stock.
+  """
+
+  @type t :: %__MODULE__ {
+    ok: boolean,
+    venue: String.t,
+    symbol: String.t,
+    bids: Dealer.Stock.bids,
+    asks: Dealer.Stock.asks,
+    ts: String.t
+  }
+
+  @type bids :: [%{ price: integer, qty: integer, isBuy: boolean }]
+  @type asks :: [%{ price: integer, qty: integer, isBuy: boolean }]
+
+  defstruct ok: false,
+            venue: "",
+            symbol: "",
+            bids: [],
+            asks: [],
+            ts: ""
+end
+
--- lib/dealer/stock.ex
+++ lib/dealer/stock.ex
@@ -5,25 +5,6 @@ defmodule Dealer.Stock do
 
   @endpoint "/ob/api/venues/:venue/stocks/:stock"
 
-  @type t :: %__MODULE__ {
-    ok: boolean,
-    venue: String.t,
-    symbol: String.t,
-    bids: Dealer.Stock.bids,
-    asks: Dealer.Stock.asks,
-    ts: String.t
-  }
-
-  @type bids :: [%{ price: integer, qty: integer, isBuy: boolean }]
-  @type asks :: [%{ price: integer, qty: integer, isBuy: boolean }]
-
-  defstruct ok: false,
-            venue: "",
-            symbol: "",
-            bids: [],
-            asks: [],
-            ts: ""
-
   import Dealer, only: [build_path: 2]
 
   @spec orderbook(String.t, String.t) :: Dealer.Response | {:error, term}
@@ -31,6 +12,6 @@ defmodule Dealer.Stock do
   def orderbook(venue, stock) do
     build_path(@endpoint, %{venue: venue, stock: stock})
     |> Dealer.get
-    |> Dealer.Response.new(%{as: __MODULE__})
+    |> Dealer.Response.new(%{as: Dealer.Stock.Orderbook})
   end
 end
