1062f99100edbcf5f77aedb809415f36ea2dd430
Larry Lv <larrylv1990@gmail.com>
2015-12-22 01:36:49 +0800

    Bump version to 0.3.1

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Dealer.Mixfile do
 
   def project do
     [app: :dealer,
-     version: "0.3.0",
+     version: "0.3.1",
      description: "An API Client for Stockfighter.",
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
